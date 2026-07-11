5c8ae2017d258e273e396d4d5ed819d487b7c56b
tmdefi <0xtmdefi@gmail.com>
2026-07-11 04:46:03 +0000

    Reset scroll position to top on page navigation, preserving it for back/forward

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useLocation, useNavigationType } from 'react-router-dom';
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+  const navigationType = useNavigationType();
+
+  useEffect(() => {
+    if (navigationType !== 'POP') {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, navigationType]);
+
+  return null;
+}
