64b982c47073c38a324c4782ed82cbd71b6090c6
tmdefi <0xtmdefi@gmail.com>
2026-08-01 22:03:54 +0000

    Revert "Detect Telegram webview layout"
    This reverts commit c39dce72c42becf071c5421e11ac99a355e6e9d8.

--- src/main.tsx
+++ src/main.tsx
@@ -19,39 +19,8 @@ type WagmiProviderConfig = ComponentProps<typeof WagmiProvider>['config'];
 function markInAppBrowser() {
   if (typeof navigator === 'undefined' || typeof document === 'undefined') return;
 
-  const currentWindow = window as typeof window & {
-    Telegram?: unknown;
-    TelegramWebview?: unknown;
-    TelegramWebviewProxy?: unknown;
-    TelegramWebviewProxyProto?: unknown;
-    TelegramWebviewProxyReceive?: unknown;
-  };
   const ua = navigator.userAgent || '';
-  const referrer = document.referrer || '';
-  const params = new URLSearchParams(window.location.search);
-  const hasTelegramBridge = Boolean(
-    currentWindow.Telegram ||
-    currentWindow.TelegramWebview ||
-    currentWindow.TelegramWebviewProxy ||
-    currentWindow.TelegramWebviewProxyProto ||
-    currentWindow.TelegramWebviewProxyReceive
-  );
-  const hasTelegramReferrer = /(^|\/\/)(t\.me|telegram\.me|telegram\.org)\//i.test(referrer) ||
-    /^android-app:\/\/org\.telegram\./i.test(referrer);
-  const isKnownInAppBrowser = /Telegram|Twitter|Instagram|FBAN|FBAV|Line\/|MicroMessenger|TikTok|Snapchat|LinkedInApp/i.test(ua);
-  const isLikelyAndroidWebView = /; wv\)|Version\/[\d.]+.*Chrome\/.*Mobile Safari/i.test(ua);
-  const isLikelyIosWebView = /iP(hone|od|ad)/i.test(ua) &&
-    /AppleWebKit/i.test(ua) &&
-    !/Safari|CriOS|FxiOS|EdgiOS|OPiOS/i.test(ua);
-  const forcedInAppBrowser = /^(1|true|telegram|x|twitter|instagram)$/i.test(params.get('in_app_browser') || '');
-  const isInAppBrowser = Boolean(
-    isKnownInAppBrowser ||
-    hasTelegramBridge ||
-    hasTelegramReferrer ||
-    isLikelyAndroidWebView ||
-    isLikelyIosWebView ||
-    forcedInAppBrowser
-  );
+  const isInAppBrowser = /Telegram|Twitter|Instagram|FBAN|FBAV|Line\/|MicroMessenger|TikTok|Snapchat|LinkedInApp/i.test(ua);
   document.documentElement.classList.toggle('is-in-app-browser', isInAppBrowser);
 }
 
